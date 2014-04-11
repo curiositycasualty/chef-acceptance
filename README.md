12863523a6bebdc2529de615a51565ced37356fd
Patrick Wright <patrick@getchef.com>
2014-04-11 16:38:25 -0700

    added more docs. merged tests

--- README.md
+++ README.md
@@ -4,9 +4,33 @@ another acceptance test prototype using capybara/rspec
 
 bundle install
 
-rake rspec
+rake spec
+
+
+split harness from tests
+
+move tests to webui2
+
+pulling harness as dep
+
 
 
 
 future:
+
+run in travis
+
+run in saucelabs
+
 require 'sauce/capybara'
+
+
+references:
+
+http://rubydoc.info/github/jnicklas/capybara/master
+
+https://code.google.com/p/selenium/wiki/RubyBindings
+
+
+kanban:
+https://chef.leankit.com/Boards/View/97159481#workflow-view
--- spec/features/login_spec.rb
+++ spec/features/login_spec.rb
@@ -1,12 +1,21 @@
 require 'spec_helper'
 
+
 # todo: Include a module with all page objects instantiated
 login_page = LoginPage.new
 usernav = Header::UserNav.new
+feedback_modal = FeedbackModal.new
 
 # todo: considering tracking current page automatically
 
 describe "user login", :type => :feature do
+
+  before(:all) do
+    @id = "test_#{Time.now.to_i}"
+    puts "Using id: #{@id}"
+    login_page.signup 'Patrick Wright', @id, "#{@id}@getchef.com", 'secret', @id, 'Chef'
+    usernav.sign_out
+  end
 
   after(:each) do
     usernav.sign_out
@@ -14,13 +23,25 @@ describe "user login", :type => :feature do
 
   it "allows user login" do
     # todo: data management -- coming
-    login_page.login 'patrick', 'patrick'
-    expect(page).to have_content 'Signed in aspatrick patrick'
+    login_page.login @id, 'secret'
+    expect(page).to have_content 'Signed in asPatrick Wright'
   end
 
   it "allows user to sign up" do
     # todo: data management -- coming
-    login_page.signup 'Joseph Smith', 'jsm', 'jsm@getchef.com', 'secret', 'jsm-test', 'Chef'
-    expect(page).to have_content 'Signed in asJoseph Smith'
+    now=Time.now.to_i
+    login_page.signup 'Patrick Wright2', "patrick#{now}", "patrick#{now}@getchef.com", 'secret', "pw-test#{now}", 'Chef'
+    expect(page).to have_content 'Signed in asPatrick Wright2'
+  end
+
+  it "email contained in feedback modal" do
+    login_page.login @id, 'secret'
+
+    # we do not want to have such a generic page content search. this is just an example
+    expect(page).not_to have_content 'support@getchef.com'
+    usernav.open_feedback_modal
+    expect(page).to have_content 'support@getchef.com'
+    
+    feedback_modal.close
   end
 end
