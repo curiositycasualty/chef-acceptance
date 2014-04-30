4f7da42982fcc6ac41eab49aee71d7f845c70f13
Patrick Wright <patrick@getchef.com>
2014-04-30 11:15:54 -0700

    rename _input to _text part 2

--- spec/pages/login.rb
+++ spec/pages/login.rb
@@ -4,13 +4,13 @@ module Page
   class Login < SitePrism::Page
     set_url '/login'
       
-    element :login_username_input, '#login-username'
-    element :login_password_input, '#login-password'
+    element :login_username_text, '#login-username'
+    element :login_password_text, '#login-password'
     element :login_button, 'input.btn.btn-small'
 
     def login(username, password)
-      login_username_input.set username
-      login_password_input.set password
+      login_username_text.set username
+      login_password_text.set password
       login_button.click
     end
   end
--- spec/pages/roles.rb
+++ spec/pages/roles.rb
@@ -4,8 +4,8 @@ module Page
   class Roles < SitePrism::Page
     set_url '/organizations{/org}/roles' 
 
-    element :name_input, '#create-role-name'
-    element :description_input, '#create-role-desc'
+    element :name_text, '#create-role-name'
+    element :description_text, '#create-role-desc'
     element :create_org_button, :xpath, '//button[@class="btn btn-primary" and not(@disabled)]'
     element :create_link, 'a.create'
     elements :role_grid, '#list-pane div.slick-cell.l0.r0'
@@ -17,8 +17,8 @@ module Page
 
       wait_for_role_modal
 
-      name_input.set name
-      description_input.set description
+      name_text.set name
+      description_text.set description
       
       wait_for_create_org_button
       create_org_button.click
