09fb4ba47536287cdee3d936416672bb5430c724
Patrick Wright <patrick@getchef.com>
2014-04-30 11:15:05 -0700

    rename _input to _text

--- spec/pages/sign_up.rb
+++ spec/pages/sign_up.rb
@@ -6,11 +6,11 @@ module Page
     set_url '/signup'
 
     # Sign Up landing page
-    element :signup_fullname_input, '#signup-display-name'
-    element :signup_username_input, '#signup-username'
-    element :signup_email_input, '#signup-email'
-    element :signup_password_input, '#signup-password'
-    element :signup_company_input, '#signup-company'
+    element :signup_fullname_text, '#signup-display-name'
+    element :signup_username_text, '#signup-username'
+    element :signup_email_text, '#signup-email'
+    element :signup_password_text, '#signup-password'
+    element :signup_company_text, '#signup-company'
     element :signup_button, 'button.btn.btn-primary'
 
     # Welcome modal - Create New Org or Accept Invite
@@ -23,11 +23,11 @@ module Page
     section :create_org_modal, Modal::CreateOrg, 'div.modal.in'
     
     def sign_up(fullname, username, email, password, org_full, org_short, options = {})
-      signup_fullname_input.set fullname
-      signup_username_input.set username
-      signup_email_input.set email
-      signup_password_input.set password
-      signup_company_input.set options[:company] if options[:company]
+      signup_fullname_text.set fullname
+      signup_username_text.set username
+      signup_email_text.set email
+      signup_password_text.set password
+      signup_company_text.set options[:company] if options[:company]
       signup_button.click
 
       create_new_org_button.click
