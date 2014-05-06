04c040a5d320a88f387fdd2aa15d881c413dbae4
Patrick Wright <patrick@getchef.com>
2014-05-06 09:15:59 -0700

    moved page objects to lib, moved misc work dirs to work

--- Rakefile
+++ Rakefile
@@ -1,11 +1,7 @@
 require "rspec/core/rake_task"
 
 chef_test_dir = ENV['CHEF_TEST_DIR'] = File.dirname(__FILE__)
-
-task :bs do
-   system("#{ENV['CHEF_TEST_DIR']}/bin/chef-client -z -l debug -o client::local")
-end
-
+work_dir = ENV['CHEF_TEST_WORK'] = "#{chef_test_dir}/work"
 
 task :default => [:spec]
 RSpec::Core::RakeTask.new do |task|
@@ -15,7 +11,9 @@ end
 
 desc 'Bootstrap browser environment and start pry'
 task :console do
-  $LOAD_PATH << './spec'
+  ['./spec', './lib'].each { |d| 
+    $LOAD_PATH << d
+  }
   
   require 'spec_helper'
   Capybara.visit Capybara.app_host
@@ -26,16 +24,16 @@ end
 
 desc 'Install required Gems into the vendor/bundle directory'
 task :bundle do
-  system('bundle install --path vendor/bundle --binstubs')
+  system("bundle install --path work/vendor/bundle --binstubs")
 end
 
 desc 'Clean bundled gems dir then install required Gems into the vendor/bundle directory'
 task :rebundle do
-  system("rm -rf vendor/bundle")
+  system("rm -rf work/vendor/bundle")
   Rake::Task['bundle'].execute
 end
 
 task :berks_install do
-  cookbooks_path = File.join(chef_test_dir, 'vendor/cookbooks')
+  cookbooks_path = File.join(chef_test_dir, "work/vendor/cookbooks")
   system("#{chef_test_dir}/bin/berks vendor #{cookbooks_path}")
 end
--- spec/scenarios/jsm_spec.rb
+++ spec/scenarios/jsm_spec.rb
@@ -7,8 +7,7 @@ require 'pages/users'
 require 'pages/organizations'
 require 'lib/util'
 require 'pages/login'
-
-require 'tempfile'
+require 'fileutils'
 
 include Navigation
 
@@ -25,67 +24,10 @@ feature 'Node roles converge associated recipes', :type => :feature do
     @ts = Time.now.to_i.to_s
   end
 
-  scenario '' do
-    pending
-    sign_up_page.load
-    sign_up_page.sign_up "full#{ts}", "user#{ts}", "email#{ts}@getchef.com", 'password', "org#{ts}", "org#{ts}"
-
-    expect(page).to have_content "Signed in asfull#{ts}"
-
-    ENV['CURRENT_ORG'] = "org#{ts}"
-    ENV['CURRENT_USER'] = "user#{ts}"
-
-    puts ENV['CURRENT_ORG'] = "org#{ts}"
-
-    # option 1 - navigate links
-    Header.go_to_policy
-    Policy.go_to_roles
-
-    #option 2 - direct page load
-    # roles_page.load(org: ts)
-
-    roles_page.create 'happyrole', "Role: #{ts}"
-
-    #expect(roles_page.find_role('happyrole').text).to have_content 'happyrole'
-
-    Policy.go_to_clients
-
-    ##
-    valid_key = clients_page.create_client("new-validator", {:validation => true})
-    file = Tempfile.new "new-validator"
-    file.write valid_key
-    file.close
-    ENV['VALID_KEY'] = file.path
-
-    puts "KEY ----------------------- #{valid_key}"
-
-    client_key = clients_page.create_client "new-client"
-    file = Tempfile.new "new-client"
-    file.write client_key
-    file.close
-    ENV['CLIENT_KEY'] = file.path
-
-    Header.go_to_administration
-    Administration.go_to_users
-
-    users_page.select_user "user#{ts}"
-    user_key = users_page.reset_key
-    file = Tempfile.new "user"
-    file.write user_key
-    file.close
-    ENV['USER_KEY'] = file.path
-
-    system("#{ENV['CHEF_TEST_DIR']}/bin/chef-client -z -l debug -o client::vagrant,client::cleanup,client::machines")
-
-    system("#{ENV['CHEF_TEST_DIR']}/bin/knife upload cookbooks")
-
-  end
-
-
   scenario "new workflow with #{@ts}" do
     puts @ts
 
-    # log in with existing user and client.pem
+    # log in with existing user
     login_page.load
     login_page.login 'chef', 'password'
 
@@ -102,7 +44,8 @@ feature 'Node roles converge associated recipes', :type => :feature do
     # reset key and write to work/org-validator.pem
     validator_key = clients_page.reset_key
 
-    File.open("#{ENV['CHEF_TEST_DIR']}/work/#{org}-validator.pem", "w") do |f|
+    FileUtils.mkdir_p "#{ENV['CHEF_TEST_DIR']}/work/pems"
+    File.open("#{ENV['CHEF_TEST_DIR']}/work/pems/#{org}-validator.pem", "w") do |f|
       f.write "#{validator_key}"
     end
 
@@ -124,19 +67,3 @@ feature 'Node roles converge associated recipes', :type => :feature do
 
   end
 end
-
-# add a recipe to a role
-# add that role to a specific client
-# # save  
-
-
-
-# create node (chef-metal)
-# then the test 'calls out' to the node
-# # runs chef-client
-# reports back that the chef-client was successfully converged
-
-
-# assumptions
-# user exists with assigned permissions
-# 
